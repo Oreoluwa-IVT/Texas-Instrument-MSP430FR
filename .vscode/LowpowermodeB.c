b8e3bfa08bec84ce8ec2f3174ae395f6591da68e
Oreoluwa <oreoluwaosewe.oluwafemi@stu.cu.edu.ng>
2023-05-29 15:39:40 +0100

    Update

---
+++ .vscode/LowpowermodeB.c
@@ -0,0 +1,34 @@
+#include <msp430.h>
+#define STOP_WATCHDOG 0x5A80 // Stop the watchdog timer
+#define ACLK 0x0100 // Timer ACLK source
+#define UP 0x0010 // Timer UP mode
+#define ENABLE_PINS 0xFFFE // Required to use inputs and outputs
+main()
+{
+    WDTCTL = STOP_WATCHDOG; // Stop the watchdog timer
+    PM5CTL0 = ENABLE_PINS; // Required to use inputs and outputs
+    P1DIR = BIT0; // Set pin for red LED as an output
+    P1OUT = 0x00; // Make sure red LED is off to start
+    TA0CCR0 = 40000; // 40K*25us ~ 1 second to ISR
+    TA0CTL = ACLK + UP; // Set ACLK, UP MODE
+    TA0CCTL0 = CCIE; // Enable interrupt for Timer0
+    _BIS_SR(LPM0_bits | GIE); // Enter Low Power Mode 0 and activate interrupts
+    while(1);
+}
+//************************************************************************
+// Timer0 Interrupt Service Routine
+//************************************************************************
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer0_ISR (void)
+{
+    if(TA0CCR0 == 40000) // If LED was off for 1 second
+    {
+        TA0CCR0 = 1000; // then turn LED on for short time
+        P1OUT = BIT0;
+    }
+    else // else LED was on for a short time
+    {
+        TA0CCR0 = 40000; // then turn LED off for long time
+        P1OUT = 0x00;
+    }
+}
