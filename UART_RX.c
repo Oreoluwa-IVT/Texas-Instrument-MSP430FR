29cc71857929e42db946758d1e0462e8f06da6a1
Oreoluwa <oreoluwaosewe.oluwafemi@stu.cu.edu.ng>
2023-05-30 07:23:46 +0100

    Update

--- UART_RX.c
+++ UART_RX.c
@@ -34,3 +34,36 @@ main()
         }
     }// end while(1)
 }// end main()
+void select_clock_signals(void)
+{
+    CSCTL0 = 0xA500; // "Password" to access clock calibration registers
+    CSCTL1 = 0x0046; // Specifies frequency of main clock
+    CSCTL2 = 0x0133; // Assigns additional clock signals
+    CSCTL3 = 0x0000; // Use clocks at intended frequency, do not slow them down
+}
+//*********************************************************************************
+//* Used to Give UART Control of Appropriate Pins *
+//*********************************************************************************
+void assign_pins_to_uart(void)
+{
+    P4SEL1 = 0x00;             // 0000 0000
+    P4SEL0 = BIT3 | BIT2; // 0000 1100
+                               // ^^
+                               // ||
+                                // |+---- 01 assigns P4.2 to UART Transmit (TXD)
+                               // |
+                               // +----- 01 assigns P4.3 to UART Receive (RXD)
+}
+//*********************************************************************************
+//* Specify UART Baud Rate *
+//*********************************************************************************
+void use_9600_baud(void)
+{
+    UCA0CTLW0 = UCSWRST; // Put UART into SoftWare ReSeT
+    UCA0CTLW0 = UCA0CTLW0 | UART_CLK_SEL; // Specifies clock source for UART
+    UCA0BR0 = BR0_FOR_9600; // Specifies bit rate (baud) of 9600
+    UCA0BR1 = BR1_FOR_9600; // Specifies bit rate (baud) of 9600
+    UCA0MCTLW = CLK_MOD; // "Cleans" clock signal
+    UCA0CTLW0 = UCA0CTLW0 & (~UCSWRST); // Takes UART out of SoftWare ReSeT
+}
+
