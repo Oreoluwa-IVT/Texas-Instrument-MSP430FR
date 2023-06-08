0330830bea31fe25d957b7d5d26e8ea91ad255ad
Oreoluwa <oreoluwaosewe.oluwafemi@stu.cu.edu.ng>
2023-06-08 11:25:33 +0100

    Update

--- ADC.c
+++ ADC.c
@@ -20,12 +20,17 @@ main()
         // Looking for threshold of 50% of 3.3V
         // with binary equivalent of
         // 1000 0000 0000B = 0x8000
-        int a=4, b = ADC12MEM0 ;
-        transmitMessage("Pressure: ");
-      //  transmitMessage("%d\r\n",b);
-        int output = map(b, 0,4096, 0,3000);
-        transmitMessage("%d\r\n",output);
-        __delay_cycles(100000);
+         int b = ADC12MEM0; // The value of the sensor is store in  the "ADC12mem0" register then stored in the variable "b".
+         int output1 = map(b, 0,4096, 1,5); //The msp430 has a 12 bit ADC that is why the range is 4096. 
+         // it was mapped to give an output range of 1-5v because it was specified on the datasheet that the output voltage 
+         // would range from 1-5v.
+         int output2 = map(output1,1,5, 0,3000); // The values were later mapped to 0-3000 because the pressure range is 0-3000psi.
+      //   int output2 = 750*(output1-1);
+         transmitMessage("Pressure: "); // The pressure is then printed to the terminal.
+      //   transmitMessage("%d\r\n",output1);
+         transmitMessage("%d\r\n",output2); // The pressure is then printed to the terminal.
+
+        __delay_cycles(130000); //  Delay for a certain period of time . Not sure. but the value changes based on what you supply. Refer to the function definition.
        // transmitMessage("%d\n",a);
         if (ADC12MEM0 > 0x001) // If input > 1.65V
         {
@@ -51,4 +56,5 @@ void ADC_SETUP(void)
     ADC12CTL1 = ADC12_SHT_SRC_SEL; // Specify sample & hold clock source
     ADC12CTL2 = ADC12_12BIT; // 12-bit conversion results
     ADC12MCTL0 = ADC12_P92; // P9.2 is analog input
+//Timer is incremental
 }
